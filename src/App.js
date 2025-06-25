a7ee1e5fe394b414cfce6e42ef5cb50ef8fdccdb
abdulbasit7866 <abdulbasit.dev@pk.see.biz>
2025-06-25 11:56:20 +0500

    25-june

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
+import { Counter, Counter1 } from "./component/Counter";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{x}</h1>
+        {/* <button onClick={() => handleX(2)}>Update X value</button> */}
+        {/* <Counter handleX={handleX} /> */}
+        <Counter1 />
       </header>
     </div>
   );
---
+++ src/component/Counter.jsx
@@ -0,0 +1,15 @@
+export const Counter = ({ handleX }) => {
+  //   const { handleX } = props;
+  const [x, setX] = useState(0);
+  const handleX = (y) => setX(x + y);
+  return (
+    <>
+      <h1>Counter {x}</h1>
+      <button onClick={() => handleX(5)}></button>
+    </>
+  );
+};
+
+export const Counter1 = () => {
+  return <h1>Counter1</h1>;
+};
